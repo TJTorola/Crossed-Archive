526c7e4a6642ff6970070c7b56e83819cf43df39
Tyler Torola <tyler@tjt.codes>
2017-04-25 21:24:30 -0700

    Add typedef for puzzle state

---
+++ src/typedef.js
@@ -0,0 +1,119 @@
+/**
+ * A single char of a word
+ * @typedef {String} Letter
+ */
+type Letter = 'A' | 'B' | 'C'
+| 'D' | 'E' | 'F' | 'G' | 'H'
+| 'I' | 'J' | 'K' | 'L' | 'M'
+| 'N' | 'O' | 'P' | 'Q' | 'R'
+| 'S' | 'T' | 'U' | 'V' | 'W'
+| 'X' | 'Y' | 'Z';
+
+/**
+ * A single char of a guess, allowing ' ' for blank
+ * @typedef {String} Guess
+ */
+type Guess = Letter | ' ';
+
+/**
+ * A tuple representing a location on the grid
+ * @type {Array}
+ */
+type Pos = [number, number];
+
+/**
+ * A string representation of a pos for keying in a pos
+ * PosKey === Pos.join(',')
+ * @typedef {String} PosKey
+ */
+type PosKey = string;
+
+/**
+ * A set containing all the wall locations for a puzzle
+ * @typedef {Set} Walls
+ */
+type Walls = Set<PosKey>;
+
+/**
+ * A number corresponding to a Word
+ * @typedef {Number} HintKey
+ */
+type WordKey = number;
+
+/**
+ * A map of locKeys to hintKeys
+ * @typedef {Map} WordKeys
+ */
+type WordKeyMap = Map<PosKey, WordKey>;
+
+/**
+ * A hint and answer combination with the key included
+ * @typedef {Object} Word
+ */
+type Word = {|
+	key: WordKey,
+	hint: string,
+	answer: Array<Letter>,
+|};
+
+/**
+ * A set of words searchable by key
+ * @typedef {Map} WordMap
+ */
+type WordMap = Map<WordKey, Word>;
+
+/**
+ * All words in a puzzle, associated by their direction
+ * @typedef {Object} PuzzleWords
+ * @property {WordMap} down
+ * @property {WordMap} across
+ */
+type PuzzleWords = {
+	down: WordMap,
+	across: WordMap,
+};
+
+/**
+ * An object containing all data about a single puzzle
+ * @typedef {Object} Puzzle
+ * @property {String} title - User friendly title of puzzle
+ * @property {String} publisher - Group responsable for publishing puzzle
+ * @property {String} author - Original creator of puzzle
+ * @property {Date} published - A date object representing the day of publish
+ * @property {Array} size - A tuple descibing width x height of the puzzle
+ * @property {Walls} walls
+ * @property {WordKeyMap} keys
+ * @property {PuzzleWords} words
+ */
+type Puzzle = {|
+	title: string,
+	publisher: string,
+	author: string,
+	published: date,
+	size: [number, number],
+	walls: Walls,
+  keys: WordKeyMap,
+	words: PuzzleWords,
+|};
+
+/**
+ * An object containing changable properties of a cell
+ * @typedef {Object} CellState
+ * @property {Guess} guess - The guessed (and rendered) letter in the cell
+ * @property {Number} guessCount - The amount of guesses made on this cell
+ */
+type CellState = {|
+	guess: Guess,
+	guessCount: number,
+|};
+
+/**
+ * An object representing selected location on a puzzle
+ * @typedef {Object} Cursor
+ * @property {Pos} focus
+ * @property {string} direction
+ */
+type Cursor = {|
+	focus: Pos,
+	direction: 'DOWN' | 'ACROSS';
+|};

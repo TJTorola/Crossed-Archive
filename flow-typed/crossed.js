594ac449bab425ba905472b44da6e87b6be07bc3
Tyler Torola <tyler@tjt.codes>
2017-08-19 16:38:38 -0700

    Fix indentation in flowtypes

--- flow-typed/crossed.js
+++ flow-typed/crossed.js
@@ -3,12 +3,12 @@
  * @typedef {String} Letter
  */
 // prettier-ignore
-declare type Letter = 'A' | 'B' | 'C'
-| 'D' | 'E' | 'F' | 'G' | 'H'
-| 'I' | 'J' | 'K' | 'L' | 'M'
-| 'N' | 'O' | 'P' | 'Q' | 'R'
-| 'S' | 'T' | 'U' | 'V' | 'W'
-| 'X' | 'Y' | 'Z';
+declare type Letter = 'A' | 'B'
+  | 'C' | 'D' | 'E' | 'F' | 'G'
+  | 'H' | 'I' | 'J' | 'K' | 'L'
+  | 'M' | 'N' | 'O' | 'P' | 'Q'
+  | 'R' | 'S' | 'T' | 'U' | 'V'
+  | 'W' | 'X' | 'Y' | 'Z';
 
 /**
  * A single char of a guess, allowing ' ' for blank

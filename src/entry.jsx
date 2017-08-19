b0cde171cacd820afab9889edc98fd69d71f1340
Tyler Torola <tyler@tjt.codes>
2017-08-19 15:00:43 -0700

    Correct alpha

--- src/entry.jsx
+++ src/entry.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { render } from "react-dom"
-import "~/sass/entry.scss"
 import App from "~/app/root"
+import "~/sass/entry.scss"
 
 render(<App />, document.querySelector("#app"))

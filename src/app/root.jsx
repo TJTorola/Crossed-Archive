69fffb649d0a1515e5b437d351fe96b245e460c2
Tyler Torola <tyler@tjt.codes>
2017-08-22 21:33:09 -0700

    Re-organize app structure

--- src/app/root.jsx
+++ src/app/root.jsx
@@ -1,9 +1,8 @@
 import React from "react"
 import { Provider } from "react-redux"
 import { createStore } from "redux"
-import Middleware from "~/middleware/index"
-import Reducers from "~/reducers/index"
-import Root from "~/view/root"
+import Middleware from "~/middleware/index.js"
+import Reducers from "~/reducers/index.js"
 
 const store = createStore(Reducers, {}, Middleware)
 global.s = store
--- src/entry.jsx
+++ src/entry.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { render } from "react-dom"
-import App from "~/app/root"
+import App from "~/app/root.jsx"
 import "~/sass/entry.scss"
 
 render(<App />, document.querySelector("#app"))
